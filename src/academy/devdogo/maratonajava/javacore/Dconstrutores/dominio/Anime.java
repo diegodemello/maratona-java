a39c1a4b563ea67b57a7500887ee07bf0f0b8902
Diego de Mello <diegodemello027@gmail.com>
2025-02-02 14:52:57 -0300

    Construtores - sobrecarga de construtor

--- src/academy/devdogo/maratonajava/javacore/Dconstrutores/dominio/Anime.java
+++ src/academy/devdogo/maratonajava/javacore/Dconstrutores/dominio/Anime.java
@@ -5,22 +5,32 @@ public class Anime {
     private String tipo;
     private int episodios;
     private String genero;
+    private String estudio;
 
     public Anime(String nome, String tipo, int episodios, String genero){
+        this();
         this.nome = nome;
         this.tipo = tipo;
         this.episodios = episodios;
         this.genero = genero;
     }
-    public Anime(){
 
+    public Anime(String nome, String tipo, int episodios, String genero, String estudio){
+        this(nome, tipo, episodios, genero);
+        this.estudio = estudio;
     }
+
+    public Anime(){
+        System.out.println("Construtor sem argumentos");
+    }
+
 
     public void imprime(){
         System.out.println(this.nome);
         System.out.println(this.tipo);
         System.out.println(this.episodios);
         System.out.println(this.genero);
+        System.out.println(this.estudio);
     }
 
     public void setGenero(String genero) {
--- src/academy/devdogo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
+++ src/academy/devdogo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
@@ -5,10 +5,7 @@ import academy.devdogo.maratonajava.javacore.Dconstrutores.dominio.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args) {
-        Anime anime = new Anime("Naruto", "TV", 30, "Ação");
-        Anime anime2 = new Anime();
+        Anime anime = new Anime("Naruto", "TV", 30, "Ação", "Nautica 3");
         anime.imprime();
-        System.out.println();
-        anime2.imprime();
     }
 }
