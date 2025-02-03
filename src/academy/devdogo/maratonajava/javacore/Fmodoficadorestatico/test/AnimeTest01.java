5451cb7ec686f49c83098de4d0c664d536cd9490
Diego de Mello <diegodemello027@gmail.com>
2025-02-02 21:41:01 -0300

    Bloco de inicialização estático

---
+++ src/academy/devdogo/maratonajava/javacore/Fmodoficadorestatico/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package academy.devdogo.maratonajava.javacore.Fmodoficadorestatico.test;
+
+import academy.devdogo.maratonajava.javacore.Fmodoficadorestatico.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime1 = new Anime();
+        Anime anime2 = new Anime();
+        Anime anime3 = new Anime();
+    }
+}
