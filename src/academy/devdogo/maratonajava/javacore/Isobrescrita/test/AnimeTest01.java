f46f5401d2a064916c18a3e9904ec56c1af4a4f3
Diego de Mello <diegodemello027@gmail.com>
2025-02-09 09:51:02 -0300

    Herança - Sobrescrita do método toString

---
+++ src/academy/devdogo/maratonajava/javacore/Isobrescrita/test/AnimeTest01.java
@@ -0,0 +1,10 @@
+package academy.devdogo.maratonajava.javacore.Isobrescrita.test;
+
+import academy.devdogo.maratonajava.javacore.Isobrescrita.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Boku no Hero");
+        System.out.println(anime);
+    }
+}
