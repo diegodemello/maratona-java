c871fe1de53501aa26cec70142f5f8105eb593e4
Diego de Mello <diegodemello027@gmail.com>
2025-02-09 21:13:40 -0300

    Modificador Final - tipo referência

--- src/academy/devdogo/maratonajava/javacore/Jmodificadorfinal/test/CarroTest01.java
+++ src/academy/devdogo/maratonajava/javacore/Jmodificadorfinal/test/CarroTest01.java
@@ -1,10 +1,16 @@
 package academy.devdogo.maratonajava.javacore.Jmodificadorfinal.test;
 
 import academy.devdogo.maratonajava.javacore.Jmodificadorfinal.dominio.Carro;
+import academy.devdogo.maratonajava.javacore.Jmodificadorfinal.dominio.Comprador;
 
 public class CarroTest01 {
     public static void main(String[] args) {
         Carro carro = new Carro();
+        Comprador comprador2 = new Comprador();
+
         System.out.println(Carro.VELOCIDADE_LIMITE);
+        System.out.println(carro.COMPRADOR);
+        carro.COMPRADOR.setNome("Diego");
+        System.out.println(carro.COMPRADOR);
     }
 }
