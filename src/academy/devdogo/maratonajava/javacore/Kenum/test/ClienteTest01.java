990afd80170f1497408a44b401ba1e85f07aa6c4
Diego de Mello <diegodemello027@gmail.com>
2025-02-11 21:27:07 -0300

    Enumeração - Construtores e atributos

--- src/academy/devdogo/maratonajava/javacore/Kenum/test/ClienteTest01.java
+++ src/academy/devdogo/maratonajava/javacore/Kenum/test/ClienteTest01.java
@@ -5,12 +5,10 @@ import academy.devdogo.maratonajava.javacore.Kenum.dominio.TipoCliente;
 
 public class ClienteTest01 {
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("Diego", TipoCliente.PESSOA_FISICA);
-        Cliente cliente2 = new Cliente("Igor", TipoCliente.PESSOA_JURIDICA);
-        Cliente cliente3 = new Cliente("Leandro", TipoCliente.PESSOA_JURIDICA);
+        Cliente cliente1 = new Cliente("Diego", TipoCliente.PESSOA_FISICA, Cliente.TipoPagamento.CREDITO);
+        Cliente cliente3 = new Cliente("Ogeid", TipoCliente.PESSOA_JURIDICA, Cliente.TipoPagamento.CREDITO);
 
         System.out.println(cliente1.toString());
-        System.out.println(cliente2.toString());
         System.out.println(cliente3.toString());
     }
 }
