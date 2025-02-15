57efd2c368356dc3c72b1bdc42daf75167d397b1
Diego de Mello <diegodemello027@gmail.com>
2025-02-14 22:34:08 -0300

    Interfaces - Atributos e métodos estáticos

--- src/academy/devdogo/maratonajava/javacore/Minterfaces/dominio/DataLoader.java
+++ src/academy/devdogo/maratonajava/javacore/Minterfaces/dominio/DataLoader.java
@@ -1,8 +1,13 @@
 package academy.devdogo.maratonajava.javacore.Minterfaces.dominio;
 
 public interface DataLoader {
+    public static final int MAX_DATA_SIZE = 10;
     public abstract void load();
     default void checkPermission(){
         System.out.println("Fazendo checagem de permissões.");
+    }
+
+    public static void retriveMaxDataSize(){
+        System.out.println("Dentro do retriveMaxDataSize na interface");
     }
 }
