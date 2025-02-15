9ae667df184c80e0d8dae676527f6dcb0e559a45
Diego de Mello <diegodemello027@gmail.com>
2025-02-15 16:14:45 -0300

    Polimorfismo - Parâmetros polimórficos

--- src/academy/devdogo/maratonajava/javacore/Npolimorfismo/sevico/CalculadoraImposto.java
+++ src/academy/devdogo/maratonajava/javacore/Npolimorfismo/sevico/CalculadoraImposto.java
@@ -1,22 +1,15 @@
 package academy.devdogo.maratonajava.javacore.Npolimorfismo.sevico;
 
 import academy.devdogo.maratonajava.javacore.Npolimorfismo.dominio.Computador;
+import academy.devdogo.maratonajava.javacore.Npolimorfismo.dominio.Produto;
 import academy.devdogo.maratonajava.javacore.Npolimorfismo.dominio.Tomate;
 
 public class CalculadoraImposto {
-    public static void calcularImpostoComputador(Computador computador){
-        System.out.println("Relatório de imposto do computador");
-        double imposto = computador.calcularImposto();
-        System.out.println("Computador: " + computador.getNome());
-        System.out.println("Valor do computador: " + computador.getValor());
-        System.out.println("Imposto a ser pago: " + imposto);
-    }
-
-    public static void calcularImpostoTomate(Tomate tomate){
-        System.out.println("Relatório de imposto do computador");
-        double imposto = tomate.calcularImposto();
-        System.out.println("Tomate: " + tomate.getNome());
-        System.out.println("Valor do tomate: " + tomate.getValor());
+    public static void calcularImposto(Produto produto){
+        System.out.println("Calculando o imposto");
+        double imposto = produto.calcularImposto();
+        System.out.println("Produto: " + produto.getNome());
+        System.out.println("Valor: " + produto.getValor());
         System.out.println("Imposto a ser pago: " + imposto);
     }
 }
--- src/academy/devdogo/maratonajava/javacore/Npolimorfismo/test/ProdutoTest01.java
+++ src/academy/devdogo/maratonajava/javacore/Npolimorfismo/test/ProdutoTest01.java
@@ -1,6 +1,7 @@
 package academy.devdogo.maratonajava.javacore.Npolimorfismo.test;
 
 import academy.devdogo.maratonajava.javacore.Npolimorfismo.dominio.Computador;
+import academy.devdogo.maratonajava.javacore.Npolimorfismo.dominio.Televisao;
 import academy.devdogo.maratonajava.javacore.Npolimorfismo.dominio.Tomate;
 import academy.devdogo.maratonajava.javacore.Npolimorfismo.sevico.CalculadoraImposto;
 
@@ -8,9 +9,12 @@ public class ProdutoTest01 {
     public static void main(String[] args) {
         Computador computador = new Computador("MacBook", 20000);
         Tomate tomate = new Tomate("Tomate cereja", 2);
+        Televisao tv = new Televisao("Smart TV 50\" ", 2500);
 
-        CalculadoraImposto.calcularImpostoComputador(computador);
+        CalculadoraImposto.calcularImposto(computador);
         System.out.println("----------------------------");
-        CalculadoraImposto.calcularImpostoTomate(tomate);
+        CalculadoraImposto.calcularImposto(tomate);
+        System.out.println("----------------------------");
+        CalculadoraImposto.calcularImposto(tv);
     }
 }
