20a95a9c53d568b0a0bde035f0f3bd7484f0437c
Diego de Mello <diegodemello027@gmail.com>
2025-02-14 22:22:01 -0300

    Interfaces - Implementando múltiplas interfaces

--- src/academy/devdogo/maratonajava/javacore/Minterfaces/dominio/DataLoader.java
+++ src/academy/devdogo/maratonajava/javacore/Minterfaces/dominio/DataLoader.java
@@ -2,4 +2,7 @@ package academy.devdogo.maratonajava.javacore.Minterfaces.dominio;
 
 public interface DataLoader {
     public abstract void load();
+    default void checkPermission(){
+        System.out.println("Fazendo checagem de permissões.");
+    }
 }
---
+++ src/academy/devdogo/maratonajava/javacore/Minterfaces/dominio/DataRemover.java
@@ -0,0 +1,5 @@
+package academy.devdogo.maratonajava.javacore.Minterfaces.dominio;
+
+public interface DataRemover {
+    public abstract void remove();
+}
